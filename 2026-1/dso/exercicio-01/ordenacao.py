b6963a7769f7758c3ae597e6be09ad141e8e6865
Lucas Keller da Silva <lucaskeller27@gmail.com>
2026-05-05 15:28:41 -0300

    continua exercício 01 de DSO

--- 2026-1/dso/exercicio-01/ordenacao.py
+++ 2026-1/dso/exercicio-01/ordenacao.py
@@ -1,28 +1,26 @@
 class Ordenacao():
 
-    def ordena(self, array_para_ordenar: list):
-        lista_ordenada = []
-        numero = array_para_ordenar[0]
-        for item in array_para_ordenar:
-            if item < numero:
-                numero = item
-            lista_ordenada.append(numero)
-            array_para_ordenar.remove(numero)
-        
-        print(lista_ordenada)
-        
-        """Realiza a ordenacao do conteudo do array recebido como parâmetro"""
-        ...
-        return ...
+    def ordena(array_para_ordenar: list):
+        n = len(array_para_ordenar)
 
-    def to_string(self, array_ordenado: list):
-        """Converte o conteudo do array em String formatado
-           Exemplo: 
-           Para o conteudo do array: [1,2,3,4,5]
-           Retorna: "1,2,3,4,5"
-           @return String com o conteudo do array formatado
-     """
-        ...
-        return ...
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if array_para_ordenar[j] > array_para_ordenar[j + 1]:
+                    array_para_ordenar[j], array_para_ordenar[j + 1] = array_para_ordenar[j + 1], array_para_ordenar[j]
+        
+        # print(array_para_ordenar)
+        return array_para_ordenar
+
+    def to_string(array_ordenado: list):
+        array_formatado = ""
+        for i in array_ordenado:
+            if array_ordenado.index(i) > 0:
+                array_formatado += f", {str(i)}"
+            else: 
+                array_formatado += str(i)
+                
+        # print(array_formatado)
+        return array_formatado
     
 Ordenacao.ordena([4, 3, 2, 1, 5])
+Ordenacao.to_string([1, 2, 3, 4, 5])
