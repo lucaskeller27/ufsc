3e8b89ddc99be58f19bcca3e3d2b5cc93d29aefe
Lucas Keller da Silva <lucaskeller27@gmail.com>
2026-05-25 18:19:27 -0300

    corrigir exercicio 01 de DSO

--- 2026-1/dso/exercicio-01/ordenacao.py
+++ 2026-1/dso/exercicio-01/ordenacao.py
@@ -1,26 +1,31 @@
 class Ordenacao():
 
-    def ordena(array_para_ordenar: list):
+    # Ordena o conteúdo do array recebido como parâmetro
+    def ordena(self, array_para_ordenar: []): # type: ignore
+
         n = len(array_para_ordenar)
 
+        # Bubble Sort
         for i in range(n):
             for j in range(0, n - i - 1):
                 if array_para_ordenar[j] > array_para_ordenar[j + 1]:
-                    array_para_ordenar[j], array_para_ordenar[j + 1] = array_para_ordenar[j + 1], array_para_ordenar[j]
-        
-        # print(array_para_ordenar)
+                    array_para_ordenar[j], array_para_ordenar[j + 1] = (
+                        array_para_ordenar[j + 1],
+                        array_para_ordenar[j]
+                    )
+
         return array_para_ordenar
 
-    def to_string(array_ordenado: list):
+    # Converte o conteúdo do array em cadeia formatada
+    def to_string(self, array_ordenado: []): # type: ignore
+
         array_formatado = ""
-        for i in array_ordenado:
-            if array_ordenado.index(i) > 0:
-                array_formatado += f", {str(i)}"
-            else: 
-                array_formatado += str(i)
-                
-        # print(array_formatado)
+
+        for i in range(len(array_ordenado)):
+
+            if i > 0:
+                array_formatado += ","
+
+            array_formatado += str(array_ordenado[i])
+
         return array_formatado
-    
-Ordenacao.ordena([4, 3, 2, 1, 5])
-Ordenacao.to_string([1, 2, 3, 4, 5])
